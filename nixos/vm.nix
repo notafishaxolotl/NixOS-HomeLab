d66f87545596763ce167a88a3189f937b2d13c53
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-12 14:33:03 +0000

    Update vm.nix

--- nixos/vm.nix
+++ nixos/vm.nix
@@ -1,1 +1,44 @@
+{config, pkgs, ... }:
 
+{
+  # Enable dconf (System Management Tool)
+  programs.dconf.enable = true;
+  programs.virt-manager.enable = true;
+
+  # Add user to libvirtd group
+  users.users.acito.extraGroups = [
+    #"libvirtd"
+    "waydroid"
+  ];
+
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    #spice spice-gtk
+    #spice-protocol
+    virtio-win
+    #win-spice
+    adwaita-icon-theme
+    qemu_kvm
+    dmidecode
+    wineWowPackages.staging
+    winetricks
+    bottles
+  ];
+
+  # Manage the virtualisation services
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        #ovmf.enable = true;
+        #ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+    waydroid.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+}

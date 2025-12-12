075c10fcf75c71c15a6afbd217db88dbad43dc06
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-12 14:33:20 +0000

    Update drives.nix

--- nixos/drives.nix
+++ nixos/drives.nix
@@ -1,1 +1,14 @@
-
+{ config, pkgs, ... }:
+{
+  fileSystems."/mnt/DoubleDisk" = {
+    device = "/dev/disk/by-uuid/b15b7d74-f1fc-410f-b5a1-1a4f36afd109";
+    fsType = "ext4";
+    options = [ 
+      "nofail"
+      "rw"
+      "exec"
+      "user_xattr"
+      "acl"
+    ];
+  };
+}

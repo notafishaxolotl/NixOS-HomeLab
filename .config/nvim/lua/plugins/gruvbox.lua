836696efd39c88e77dbbb3d7606b0f4a56ab7d98
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-20 13:33:04 +0000

    Add files via upload

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    name = "gruvbox",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
+      vim.cmd.colorscheme "gruvbox"
+      
+      vim.cmd([[
+        highlight NormalFloat guibg=NONE
+        highlight FloatBorder guibg=NONE
+      ]])
+    end
+  }
+}

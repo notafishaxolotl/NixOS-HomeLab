94637ad20d57db7a081b584e683b015934400e54
notafishaxolotl <johnmulwa.0007@aol.com>
2025-12-21 20:07:09 +0000

    homepage

--- nixos/web/homepage.nix
+++ nixos/web/homepage.nix
@@ -3,5 +3,34 @@
 {
   services.homepage-dashboard = {
     enable = true;
+
+    settings = {
+      title = "The Center";
+      cardBlur = "sm";
+    };
   };
+
+  services = [{
+    "Infrastructure" = [
+      {
+          "Proxmox" = {
+            icon = "proxmox.png";
+            href = "https://192.168.1.100:8006";
+            description = "Hypervisor";
+          }
+        }
+      ];
+    };
+  ];
+
+  widgets = [
+    {
+      resources = {
+        cpu = true;
+        memory = true;
+        disk = "/";
+      };
+    }
+  ];
+  networking.firewall.allowedTCPPorts = [ 8082 ];
 }

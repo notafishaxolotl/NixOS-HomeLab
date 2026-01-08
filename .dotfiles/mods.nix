247df22dd8fb541006b3cf228135681b27209ba6
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2026-01-08 09:38:42 +0000

    Update mods.nix

--- .dotfiles/mods.nix
+++ .dotfiles/mods.nix
@@ -1,1 +1,56 @@
+{ config, pkgs, lib, ... }:
 
+{
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+
+    servers.fabric = {
+      enable = true;
+      declarative = true;
+
+      package = pkgs.fabricServers.fabric-1_21_1.override {
+        loaderVersion = "0.16.10";
+      }; 
+
+      serverProperties = {
+        server-port = 43000;
+        difficulty = 2;
+        gamemode = 0;
+        max-players = 5;
+        motd = "WuluH";
+        white-list = true;
+        allow-cheats = false;
+      };
+
+      operators = {
+      # Use https://mcuuid.net/ to get a Minecraft UUID for a username
+        xr_lynx = {
+          "b55d65b2-3aea-4a29-968c-0a405b5a9d26";
+          level = 4;
+          bypassesPlayerLimit = true;
+        };
+      };
+
+      whitelist = {
+        xr_lynx = "b55d65b2-3aea-4a29-968c-0a405b5a9d26";
+      };
+
+      symlinks = {
+        mods = pkgs.linkFarmFromDrvs "mods" (
+          builtins.attrValues {
+            Fabric-API = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/9YVrKY0Z/fabric-api-0.115.0%2B1.21.1.jar";
+              sha512 = "e5f3c3431b96b281300dd118ee523379ff6a774c0e864eab8d159af32e5425c915f8664b1cd576f20275e8baf995e016c5971fea7478c8cb0433a83663f2aea8";
+            };
+            Backpacks = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/MGcd6kTf/versions/Ci0F49X1/1.2.1-backpacks_mod-1.21.2-1.21.3.jar";
+              sha512 = "6efcff5ded172d469ddf2bb16441b6c8de5337cc623b6cb579e975cf187af0b79291b91a37399a6e67da0758c0e0e2147281e7a19510f8f21fa6a9c14193a88b";
+            };
+          }
+        );
+      };
+    };
+  };
+}

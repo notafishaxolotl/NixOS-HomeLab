7c5822d3895ac2329b0673313063946ad2f7bb03
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2026-01-08 09:37:49 +0000

    Refactor serverProperties in minecraft.nix
    Removed duplicate serverProperties configuration.

--- .dotfiles/minecraft.nix
+++ .dotfiles/minecraft.nix
@@ -14,6 +14,16 @@
         loaderVersion = "0.16.10";
       }; 
 
+      serverProperties = {
+        server-port = 43000;
+        difficulty = 2;
+        gamemode = 0;
+        max-players = 5;
+        motd = "WuluH";
+        white-list = true;
+        allow-cheats = false;
+      };
+
       operators = {
       # Use https://mcuuid.net/ to get a Minecraft UUID for a username
         xr_lynx = {
@@ -25,16 +35,6 @@
 
       whitelist = {
         xr_lynx = "b55d65b2-3aea-4a29-968c-0a405b5a9d26";
-      };
-
-      serverProperties = {
-        server-port = 43000;
-        difficulty = 2;
-        gamemode = 0;
-        max-players = 5;
-        motd = "WuluH";
-        white-list = true;
-        allow-cheats = false;
       };
 
       symlinks = {
